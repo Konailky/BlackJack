cf20b62043939da438b90cf803ddbe2a03f96549
Konailky <32402567+Konailky@users.noreply.github.com>
2017-10-02 14:36:31 -0700

    10.2.17
    You need to work on this

--- Game.java
+++ Game.java
@@ -33,39 +33,44 @@ public class Game
                     playerTotal = playerTotal + player.getNew();
                     System.out.println("Your new total is " + (playerTotal) + "."); 
                     if (playerTotal > 21){
-                        bust = true;
+                        System.out.println("You have busted and you lose :P");
+                        check = 0;
+                        money -= 1;
                     }
                 }
 
                 else if (answer.equals("stay")){
                     System.out.println("Your total will now be compared to the dealer.");
-                    check -= 1;
-                }
-                while (dealerTotal < 17){
-                    System.out.println("Dealer drew a total of " + (dealerTotal) + ".");
-                    System.out.println("Dealer will draw until they have at least 17");
-                    dealer.Hit();
-                    dealerTotal = dealerTotal + dealer.getNew();
-                    System.out.println("Dealer's new total is " + (dealerTotal + dealer.getNew()) + ".");
-                    if (dealerTotal > 21){
-                        bust = true;
+                    check = 0;
+                    while (dealerTotal < 17){
+                        System.out.println("Dealer drew a total of " + (dealerTotal) + ".");
+                        System.out.println("Dealer will draw until they have at least 17");
+                        dealer.Hit();
+                        dealerTotal = dealerTotal + dealer.getNew();
+                        System.out.println("Dealer's new total is " + (dealerTotal + dealer.getNew()) + ".");
+                        if (dealerTotal > 21){
+                            System.out.println("Dealer has busted and you win :D");
+                            check = 0;
+                            money += 2;
+                        }
                     }
+
                 }
 
-                if (bust == true){
-                    if( playerTotal > dealerTotal){
-                        System.out.println("Your total was " + playerTotal + " and dealer's total was " + dealerTotal +".");
-                        System.out.println("You have won :D");
-                        money +=3;
-                    }
-                    else if (playerTotal < dealerTotal) {
-                        System.out.println("Your total was " + playerTotal + " and dealer's total was " + dealerTotal +".");
-                        System.out.println("You have lost :P");
-                        money -=1;
-                    }
+            }
+            if (bust == false){
+                if( playerTotal > dealerTotal){
+                    System.out.println("Your total was " + playerTotal + " and dealer's total was " + dealerTotal +".");
+                    System.out.println("You have won :D");
+                    money +=3;
+                }
+                else if (playerTotal < dealerTotal) {
+                    System.out.println("Your total was " + playerTotal + " and dealer's total was " + dealerTotal +".");
+                    System.out.println("You have lost :P");
+                    money -=1;
                 }
             }
-
+     
+           }
         }
     }
-}
